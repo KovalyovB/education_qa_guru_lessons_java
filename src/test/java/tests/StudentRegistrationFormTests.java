db6ffc33fe03a7f22888ff0ba4a60bcde285ef7f
KovalyovB <thabuz555@gmail.com>
2026-06-02 02:52:56 +0300

    added changes to StudentRegistrationFormTests

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -1,4 +1,43 @@
 package tests;
 
-public class StudentRegistrationFormTests {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+public class StudentRegistrationFormTests extends TestBase {
+    @Test
+    void RegistrationWithAllFieldsTest() {
+        open("/automation-practice-form");
+        $("[id='firstName']").setValue("Антон");
+        $("[id='lastName']").setValue("Корягин");
+        $("[id='userEmail']").setValue("koryaginant@google.com");
+        $("[id='genterWrapper'] [value='Male']").click();
+        $("[id='userNumber']").setValue("+948581928");
+        $("[id='dateOfBirthInput']").click();
+        $("[class='react-datepicker__month-select']").selectOption("February");
+        $("[class='react-datepicker__year-select']").selectOption("1996");
+        $("[class='react-datepicker__day react-datepicker__day--003 react-datepicker__day--weekend']").click();
+        $("[class='subjects-auto-complete__control css-13cymwt-control']").click();
+        $("[class='subjects-auto-complete__input']").setValue("Bio").pressEnter();
+        $("[id='hobbiesWrapper']").$(byText("Sports")).click();
+        $("[id='hobbiesWrapper']").$(byText("Reading")).click();
+        $("[id='hobbiesWrapper']").$(byText("Music")).click();
+        $("input[type='file']").uploadFromClasspath("test.pdf");
+        sleep(1000000);
+
+
+        $("[#hobbies-checkbox-1]").setSelected(true);;
+        $("[class='react-datepicker__year-select']").selectOption("1996");
+        $("[class='react-datepicker__year-select']").selectOption("1996");
+        $("[class='react-datepicker__year-select']").selectOption("1996");$("[class='react-datepicker__year-select']").selectOption("1996");
+        $("[class='react-datepicker__year-select']").selectOption("1996");
+
+
+        sleep(1000000);
+    }
 }
+
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -12,10 +12,6 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-    }
-
-    @AfterEach
-    void afterEach() {
-        closeWebDriver();
+        Configuration.holdBrowserOpen = true;
     }
 }
