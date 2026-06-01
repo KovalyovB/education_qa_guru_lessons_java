091e2a5adfcf932f4fb63dec4b2a0f24fb624812
KovalyovB <thabuz555@gmail.com>
2026-06-01 21:17:49 +0300

    Modification JUnit examples  + add new test

--- src/test/java/tests/examples/JUnitExampleTests.java
+++ src/test/java/tests/examples/JUnitExampleTests.java
@@ -1,6 +1,10 @@
 package tests.examples;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class JUnitExampleTests {
     @BeforeAll
