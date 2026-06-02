791688ca1435362db46666bddeaf8d4b272bf77f
KovalyovB <thabuz555@gmail.com>
2026-06-03 00:00:39 +0300

    add negative test emailValidationFailed

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -75,6 +75,32 @@ public class StudentRegistrationFormTests extends TestBase {
 
 
     }
+
+    @Test
+    void emailValidationFailed() {
+        $("[id='firstName']").setValue("Антон");
+        $("[id='lastName']").setValue("Корягин");
+        $("[id='userEmail']").setValue("111");
+        $("[id='genterWrapper'] [value='Male']").click();
+        $("[id='userNumber']").setValue("9948581928");
+        $("[id='dateOfBirthInput']").click();
+        $("[class='react-datepicker__month-select']").selectOption("February");
+        $("[class='react-datepicker__year-select']").selectOption("1996");
+        $("[class='react-datepicker__day react-datepicker__day--003 react-datepicker__day--weekend']").click();
+        $("[class='subjects-auto-complete__control css-13cymwt-control']").click();
+        $("[class='subjects-auto-complete__input']").setValue("Bio").pressEnter();
+        $("[id='hobbiesWrapper']").$(byText("Sports")).click();
+        $("[id='hobbiesWrapper']").$(byText("Reading")).click();
+        $("[id='hobbiesWrapper']").$(byText("Music")).click();
+        $("input[type='file']").uploadFromClasspath("krolik.jpg");
+        $("[id='currentAddress']").setValue("Деревня дедушки, ул.Колотушкина 32");
+        $("[id='react-select-3-input']").setValue("Har").pressEnter();
+        $("[id='react-select-4-input']").setValue("P").pressEnter();
+        $("[id='submit']").click();
+
+        $("#example-modal-sizes-title-lg").shouldNot(exist);
+    }
 }
+
 
 
